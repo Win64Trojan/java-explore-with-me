09a3d66d88af4a3827f8a4390523ad3f8b666bb6
Aleksandr Ivanov <sasha942710@yandex.ru>
2024-12-05 21:45:23 +0300

    Добавить обработку исключений

--- stats-service/stats-server/src/main/java/ru/practicum/exception/ErrorHandler.java
+++ stats-service/stats-server/src/main/java/ru/practicum/exception/ErrorHandler.java
@@ -1,11 +1,14 @@
 package ru.practicum.exception;
 
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
@@ -25,6 +28,24 @@ public class ErrorHandler {
         return new ErrorResponse(e.getMessage(), "");
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)  // 400
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+        log.warn("Validation errors: {}", errors);
+        return errors;
+    }
 
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)  // 400
+    public Map<String, String> handleConstraintViolation(ConstraintViolationException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getConstraintViolations().forEach(violation ->
+                errors.put(violation.getPropertyPath().toString(), violation.getMessage()));
+        log.warn("Validation errors: {}", errors);
+        return errors;
+    }
 
 }
